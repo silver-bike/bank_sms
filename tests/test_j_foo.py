c92b62b9c1c7dd6143a1ccfff707ae41899dfa8b
silverbike <silverbike.python@gmail.com>
2024-06-01 16:38:34 +0400

    changes in tests

--- tests/test_j_foo.py
+++ tests/test_j_foo.py
@@ -1,8 +1,11 @@
 from src.j_foo import data_from_json
 
+
 def test_data_from_json():
-    assert data_from_json("C:/Users/podob/dev/sky-python/Bank_SMS/test.json") == [{'id': 1001, 'group': 'Cure', 'song': 'Friday'},
-                                              {'id': 1002, 'group': 'Doors', 'song': 'Fire'}]
+    assert (data_from_json("C:/Users/podob/dev/sky-python/Bank_SMS/test.json") ==
+            [{'id': 1001, 'group': 'Cure', 'song': 'Friday'},
+             {'id': 1002, 'group': 'Doors', 'song': 'Fire'}])
+
 
 def test_data_from_json_empty():
     assert data_from_json("C:/Users/podob/dev/sky-python/Bank_SMS/empty.json") == []
