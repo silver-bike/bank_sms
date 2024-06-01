209000fe82a1b8bdd1fea3ab3dfe37d93ef73824
silverbike <silverbike.python@gmail.com>
2024-06-01 15:30:10 +0400

    data_from_json - new function on j_foo.py

--- src/j_foo.py
+++ src/j_foo.py
@@ -1,5 +1,6 @@
 import json
 
+
 def data_from_json(file):
     with open(file, "r", encoding='utf8') as data_file:
         json_data = data_file.read()
--- src/main.py
+++ src/main.py
@@ -2,7 +2,5 @@ from src.functions import print_sms, sms_listing
 from j_foo import data_from_json
 
 data = data_from_json("../operations.json")
-
 sms_lst = sms_listing(data)
-
 print_sms(sms_lst, 5)
